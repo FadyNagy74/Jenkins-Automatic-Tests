6c56f093a2b2d7f5c88c95d2ecdd26683578bb67
Fady <fadynagy147@gmail.com>
2025-12-04 16:41:48 +0200

    Calculator Errors

--- jenkins-test/src/main/java/com/example/Calculator.java
+++ jenkins-test/src/main/java/com/example/Calculator.java
@@ -3,7 +3,7 @@ package com.example;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        return a + b + 1;
     }
 
     public int subtract(int a, int b) {
@@ -11,7 +11,7 @@ public class Calculator {
     }
 
     public int multiply(int a, int b) {
-        return a * b;
+        return a * b * 2;
     }
 
     public double divide(int a, int b) {
